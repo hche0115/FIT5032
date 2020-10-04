d82b09859720fac08474781865e4dd0c4056f4c3
hche0115 <70002249+hche0115@users.noreply.github.com>
2020-10-04 20:27:14 +1100

    FIT5032_Studio_ass2
    FIT5032_Studio_assessment Task2

---
+++ FIT5032_StudioAss2/Views/Model1.cs
@@ -0,0 +1,26 @@
+namespace FIT5032_Week7.Views
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=FIT5032_Models")
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Unit> Units { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasMany(e => e.Units)
+                .WithRequired(e => e.Student)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
---
+++ FIT5032_StudioAss2/dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES ('1e7a98a6-156f-4943-b5f8-73a56e7ef003', 1)
