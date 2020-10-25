75501325bdab751e14bae5f884eceb2d54a17c5c
hche0115 <70002249+hche0115@users.noreply.github.com>
2020-10-25 23:01:23 +1100

    FIT5032 Studio Access 3
    Send Email with attchment
    rich text editor

---
+++ FIT5032 studio Access3/FIT5032_Week08A/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using FIT5032_Week08A.Models;
+using FIT5032_Week08A.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FIT5032_Week08A.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Please comment out these codes once you have registered your API key.
+            //EmailSender es = new EmailSender();
+            //es.RegisterAPIKey();
+            return View(new FeedbackModel());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model, HttpPostedFileBase postedFileBase)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents, postedFileBase);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FeedbackModel feedback)
+        {
+            return View(feedback);
+        }
+    }
+}
