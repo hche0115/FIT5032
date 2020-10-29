e7b6529c29c91192f4971e1adbb03c85978819b8
hche0115 <70002249+hche0115@users.noreply.github.com>
2020-10-29 23:02:19 +1100

    fit5032_week11
    week11

---
+++ FIT5032_Week11/FIT5032_Week08A/Models/RichTextEditorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week08A.Models
+{
+    public class FeedbackModel
+    {
+        [Display(Name = "Feedback:")]
+        [Required(ErrorMessage = "Please provide feedback.")]
+        
+        [AllowHtml]
+        public string FeedbackDetails { get; set; }
+    }
+}
---
+++ FIT5032_Week11/FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "";
+
+        public void Send(String toEmail, String subject, String contents, HttpPostedFileBase postedFileBase)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("630751226@qq.com", "NextGen Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            if (postedFileBase != null && postedFileBase.ContentLength > 0)
+            { 
+                StreamReader reader = new StreamReader(postedFileBase.InputStream);
+                string s = reader.ReadToEnd();   
+                msg.AddAttachment(postedFileBase.FileName, s);
+
+            }
+
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
